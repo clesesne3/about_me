4e7cc94a7003804f677831195373d821ae883f06
Chris Lesesne <clesesne3@gmail.com>
2017-09-30 15:01:32 -0700

    for/while loops working for Question 7

--- app.js
+++ app.js
@@ -71,11 +71,11 @@ if (answer5 === 'yes') {
 }
 else {
   alert('Sorry ' + user + ', that\'s wrong. Great try though! You scored a total of ' + userPoints + ' points.');
-}
-*/
+}*/
+
 //Question #6 - Guess the number I am thinking of!(Generate random number; user will have four attempts)
 var guessNum = Math.floor((Math.random() * 10) + 1); // creates random integer between 1 and 10 inclusively
-var answer6 = prompt('Okay ' + user + ', I\'m thinking of a number between 1 and 10...can you guess which number? (Enter integer between 1 and 20)');
+var answer6 = prompt('Okay ' + user + ', I\'m thinking of a number between 1 and 10...can you guess which number? (Enter integer between 1 and 10)');
 console.log(user + ' answered ' + answer6 + ' for Question 6');
 var numTries = 0; //initialize counter for number of user guesses
 var maxTries = 4;
@@ -83,17 +83,58 @@ if (answer6 === guessNum) {
   userPoints++;
   alert('Wow! Way to go, ' + user + '! You read my mind! You have ' + userPoints + ' points.');
 }
+else if (Number.isInteger(answer6) === false) {
+  alert('You have to enter an integer!');
+  answer6 = prompt('Number between 1 and 20? (Enter integer between 1 and 20):');
+}
 else {
   while (numTries < 3) {
     numTries++;
     var remainingTries = maxTries - numTries;
     alert('Nope, guess again ' + user + '! You still have ' + remainingTries + ' left.');
-    answer6 = prompt('Number between 1 and 20? (Enter integer between 1 and 20):\n Tries left: ' + remainingTries);
+    answer6 = prompt('Number between 1 and 10? (Enter integer between 1 and 10):\n Tries left: ' + remainingTries);
   }
   alert('Don\'t beat yourself up about it, ' + user + ' :-) On to the final question!');
 }
 
 //Question #7 - Guess one of my favorite countries to visit
+var answer7 = prompt('You\'ve made it to the final question, ' + user + '. Ready? Can you guess one of the many countries I\'ve explored? (Don\'t forget, countries are proper nouns...be sure to capitalize that first letter!)');
+console.log(user + ' answered ' + answer7 + ' for Question 7');
+var countryArray = ['South Africa', 'China', 'Jamaica', 'Dubai', 'France', 'Germany', 'England', 'Luxembourg'];
+var numAttempts = 0; // initialize counter for number of user guesses
+var maxAttempts = 6; // maximum number of user guesses
+var guessCorrect = false;
+
+while(numAttempts < 5) {
+  // loop through array of countries to check user's answer against all array items
+  for (var i = 0; i < countryArray.length; i++) {
+    if (answer7 === countryArray[i]) {
+      userPoints++;
+      alert('Awesome, you got it, ' + user + '! ' + answer7 + ' is an amazing place!');
+      alert('You scored ' + userPoints + ' out of 7 points, ' + user + '!');
+      guessCorrect = true; //Boolean value to confirm correct user guess
+      break; // break out of for loop if user guesses correctly
+    }
+
+    else {
+      guessCorrect = false;
+      continue;
+    }
+  }
+
+  // break out of while loop if user guesses correctly
+  if (guessCorrect === true) {
+    break;
+  }
+
+  // give user 5 more attempts if initial guess is incorrect
+  else {
+    numAttempts++;
+    var remainingAttempts = maxAttempts - numAttempts;
+    alert('Nope, great place, but that one wasn\'t on the list. Try again!');
+    answer7 = prompt('Guess another country: \n Tries Left: ' + remainingAttempts);
+  }
+}
 
 // thank user; end guessing game
 alert('Thanks for playing and visiting my page ' + user + '!');
